c7ea52c5bdb37904ba8010433910bffd5f7cf960
arjunsinghy96 <14bcs010@smvdu.ac.in>
2017-11-22 22:54:31 +0530

    Initial Commit

---
+++ src/js/app.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+  el: "#app",
+  data: {
+    message: "Hello, this is going fine!",
+    navItems: [
+       {name : 'Home'},
+       {name : 'Projects'},
+       {name : 'Blog'}
+       ] 
+    }
+  })
