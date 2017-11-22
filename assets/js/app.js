a286e94f51cb560cf58c0f66aa7598300ed962f6
arjunsinghy96 <14bcs010@smvdu.ac.in>
2017-11-22 23:56:22 +0530

    Add component for pages
    Adds components for home, projects and blogs pages.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,15 @@
+Vue.component('home-page', {
+  template: '<p>This is the home page</p>'
+  })
+
+Vue.component('projects-page', {
+  template: '<p>This is the projects page</p>'
+  })
+
+Vue.component('blogs-page', {
+  template: '<p>This is the blogs page</p>'
+  })
+
 var app = new Vue({
   el: "#app",
   data: {
@@ -5,7 +17,14 @@ var app = new Vue({
     navItems: [
        {name : 'Home'},
        {name : 'Projects'},
-       {name : 'Blog'}
-       ] 
+       {name : 'Blogs'}
+       ],
+    currentPage: 'home'
+    },
+  methods: {
+    changePage: function(pageName) {
+      console.log(pageName);
+      this.currentPage = pageName.toLowerCase();
+      }
     }
   })
